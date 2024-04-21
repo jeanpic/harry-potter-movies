2543672501219eba270d497de9717a32be2f98d5
Jean Picard <jpicard@cross-systems.com>
2024-04-21 16:29:44 +0200

    feat: update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import {movieDetailsResolver} from "./movie-details/movie-details.resolver";
 
 export const routes: Routes = [
   {
+    path: 'movies',
+    loadComponent: () => import('./movie-list/movie-list.component').then(c => c.MovieListComponent)
+  },
+  {
     path: 'movies/:id',
     loadComponent: () => import('./movie-details/movie-details.component').then(c => c.MovieDetailsComponent),
     resolve: {
       movie: movieDetailsResolver
     }
   },
-  {path: '**', loadComponent: () => import('./movie-list/movie-list.component').then(c => c.MovieListComponent)}
+  {path: '**', redirectTo: 'movies'}
 ];
